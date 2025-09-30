b0855013616c8e76f56577199b31400897586cbb
서현준 <hhjr976@gmail.com>
2025-09-30 13:18:36 +0900

    기본 폰트 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
 import './globals.css';
+import { pretendard } from './fonts';
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='ko'>
+		<html lang='ko' className={pretendard.variable}>
 			<body>{children}</body>
 		</html>
 	);
