27bb46e6ef4f512371a8f482b5e8c9f998ecb86f
xognsm99 <sadkangmin@gmail.com>
2025-10-27 15:26:29 +0900

    feat: Help 화면 스타일/동작 수정

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
-'use client';
+"use client";
 import ExchDisplay from '../feature/ExchDisplay';
 
 export default function Home() {
-	return (
-		<>
-			<ExchDisplay />
-		</>
-	);
+  return <ExchDisplay />;
 }
