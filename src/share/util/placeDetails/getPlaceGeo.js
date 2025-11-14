a7d9f7221b5053eb391fd549ab8592ade2587289
서현준 <hhjr976@gmail.com>
2025-11-14 15:39:57 +0900

    장소 정보 가져오기 수정

--- src/share/util/placeDetails/getPlaceGeo.js
+++ src/share/util/placeDetails/getPlaceGeo.js
@@ -5,11 +5,11 @@ export default async function getPlaceGeo(placeName) {
 		const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 		const places = await getPlaceId(placeName);
 
-		if (!places.places || places.places.length === 0) {
+		if (!places || places.length === 0) {
 			throw new Error('해당 장소에 대한 정보가 없습니다');
 		}
 
-		const placeId = places.places[0].id;
+		const placeId = places[0].id;
 
 		const endPoint = `https://places.googleapis.com/v1/places/${placeId}`;
 
