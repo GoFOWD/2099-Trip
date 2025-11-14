ad65fafdc49e06509286cc65e01b5d2b30d9e6c9
서현준 <hhjr976@gmail.com>
2025-11-14 16:50:41 +0900

    투어 테이블 가격 정보 nullable로 변경

---
+++ src/app/planning/schedule/[id]/tours/components/DateSelectModal .js
@@ -0,0 +1,38 @@
+'use client';
+
+export default function DateSelectModal({
+	onClose,
+	onSelect,
+	minDate,
+	maxDate,
+	setSelectedTour,
+	item
+}) {
+	// 저장해야할 내용
+	//
+	console.log(item);
+
+	return (
+		<div className='fixed inset-0 bg-black/50 flex items-center justify-center z-50 px-4'>
+			<div className='bg-white w-full max-w-sm p-5 rounded-lg shadow-md'>
+				<h2 className='text-lg font-semibold mb-4'>날짜 선택</h2>
+
+				<input
+					type='date'
+					className='w-full border p-2 rounded mb-4'
+					min={minDate}
+					max={maxDate}
+					onChange={e => onSelect(e.target.value)}
+				/>
+
+				<div className='flex justify-end gap-2'>
+					<button
+						className='px-4 py-2 border-2 border-(--brandColor) rounded-lg'
+						onClick={onClose}>
+						<span className='text-(--brandColor)'>선택 완료</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
